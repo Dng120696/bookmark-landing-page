15ebf8c8d0c7838f1333d32dd88076e086333b5f
Dng16 <dongyang0016@gmail.com>
2023-06-22 21:36:20 +0800

    final commit

--- script.js
+++ script.js
@@ -80,6 +80,41 @@ form.addEventListener('submit', (e)=>{
  
 })
 
+////sticky Nav
+const header = document.querySelector('.header')
+const nav = document.querySelector('.nav')
+const stickyNav = (entries)=>{
+const [entry] =entries;
+    if(!entry.isIntersecting){
+      nav.classList.add('sticky')
+    }else{
+      nav.classList.remove('sticky')
+    }
+
+}
+const observer = new IntersectionObserver(stickyNav,{
+  root:null,
+  threshold:0.1,
+  rootMargin:'-180px'
+})
+observer.observe(header)
+
+
+const fadedSection = (entries)=>{
+  entries.forEach(entry =>{
+    entry.target.classList.toggle('faded',entry.isIntersecting)
+    if(entry.isIntersecting)
+    obsreverSec.unobserve(entry.target)
+  })
+}
+const obsreverSec = new IntersectionObserver(fadedSection,{
+  root:null,
+  threshold:0.2
+})
+const section  = document.querySelectorAll('section')
+section.forEach(sec =>{
+  obsreverSec.observe(sec)
+})
 
 function isEmail(email) { 
   return /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(email);
